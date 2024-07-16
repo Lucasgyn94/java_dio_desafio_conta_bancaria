7ab5fbc1965c07711d78befd8e52a388bf9032e2
Lucasgyn94 <needslucas944@gmail.com>
2024-07-16 16:11:54 -0300

    Resolução desafio Conta Bancária

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import controllers.ContaController;
+import views.ContaView;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ContaController controller = new ContaController();
+        controller.iniciar();
+    }
+}
